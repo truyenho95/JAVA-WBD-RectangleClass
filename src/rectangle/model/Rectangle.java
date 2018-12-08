782d5b5f6b9e062c9d34688fdd0397e97d0013f2
truyenho95 <truyenho.webdev@gmail.com>
2018-12-08 21:10:41 +0700

    Add rectangle source folder & .gitignore

---
+++ src/rectangle/model/Rectangle.java
@@ -0,0 +1,25 @@
+package rectangle.model;
+
+public class Rectangle {
+    private double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "rectangle.model.Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
